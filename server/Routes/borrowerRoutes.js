cac88f03f080975f8c7b63c4885bdf9b235f42fd
Gajendra sahu <82883877+Gajmain2020@users.noreply.github.com>
2023-09-18 15:46:09 +0530

    fetching post and commenting end point created

--- server/Routes/borrowerRoutes.js
+++ server/Routes/borrowerRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { postRequirement } from "../Controller/borrowerControler.js";
+import {
+  postRequirement,
+  postComment,
+  fetchPostsInitially,
+} from "../Controller/borrowerControler.js";
 import { userAuth } from "../Middleware/userAuthentication.js";
 
 const router = express.Router();
 
+router.get("/fetch-post", userAuth, fetchPostsInitially);
 router.post("/post-requirement", userAuth, postRequirement);
+router.patch("/comment", userAuth, postComment);
 
 export default router;
