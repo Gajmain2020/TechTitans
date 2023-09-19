289d86a6899238f27ae5ddeda2e1644104ec16ce
Gitesh <gitesh.sarvaiya28@gmail.com>
2023-09-19 22:49:54 +0530

    form create

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import "./app.css";
 import Footer from "./components/Footer/Footer";
 import MainBody from "./components/MainBody/MainBody";
 import Navbar from "./components/Navbar/Navbar";
+import SignUp from "./components/Signup/Signup"
+
+
 
 function App() {
   return (
     <>
-      <Navbar />
-      <MainBody />
-      <Footer />
+    <SignUp />
     </>
   );
 }
