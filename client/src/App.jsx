6a0123e71c9b0da73b9ac31bf33248f3297854f4
Gitesh <gitesh.sarvaiya28@gmail.com>
2023-09-24 13:04:17 +0530

    error resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 import Footer from "./components/Footer/Footer";
 import MainBody from "./components/MainBody/MainBody";
 import Navbar from "./components/Navbar/Navbar";
-import Post from "./components/Post";
+import Post from "./components/Post/Post";
 import SignUp from "./components/Signup/Signup";
 
 function App() {
